a63c761141bb2c9947ddb246635d61c4aca8dc36
agcrofoot <agcrofoot@crimson.ua.edu>
2020-01-24 14:13:11 -0600

    my first commit ever

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,6 +7,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Alexandra says hello");
         }
     }
 }
